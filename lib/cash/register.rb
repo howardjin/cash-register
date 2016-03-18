a50b8a32271fad3b826192c6f51b63556f546364
howardjin <hjin@CNhjin.local>
2016-03-18 17:11:55 +0800

    adding 'single item' and 'item with count' case and make them pass

--- lib/cash/register.rb
+++ lib/cash/register.rb
@@ -1,10 +1,67 @@
+require 'json'
+
+class LineItem
+  attr_accessor :product, :count
+
+  def initialize(product, count)
+    @product = product
+    @count = count
+  end
+end
 
 class Register
+
   def generateReceipts(inputs)
-    ['wrong receipt']
+    inputJson = JSON.parse(convert_to_valid_json(inputs))
+    inputJsonItem = inputJson[0]
+    lineItemParts = inputJsonItem.split('-')
+
+    if(lineItemParts[1].nil?)
+      count = 1
+    elsif
+      count = lineItemParts[1].to_i
+    end
+    receipts  = []
+    product = @items.detect { |item| item.id == lineItemParts[0]}
+    lineItem = LineItem.new(product,count)
+    add_header(receipts)
+    add_line_items(receipts, lineItem)
+    add_separator(receipts)
+    add_summary(product, receipts, count)
+    add_footer(receipts)
+    receipts
+  end
+
+  def convert_to_valid_json(inputs)
+    inputs.tr("'", '"')
+  end
+
+  def add_line_items(receipts, lineItem)
+    receipts << "名称:#{lineItem.product.name},数量:#{lineItem.count}瓶,单价:#{formatPrice(lineItem.product.unit_price)}(元),小计:#{formatPrice(lineItem.product.unit_price * lineItem.count)}(元)"
+  end
+
+  def add_summary(product, receipts, count)
+    receipts << "总计:#{formatPrice(product.unit_price * count)}(元)"
+  end
+
+  def add_separator(receipts)
+    receipts << '------------------'
+  end
+
+  def add_footer(receipts)
+    receipts << '******************'
+  end
+
+  def add_header(receipts)
+    receipts << '***<没赚钱商店>购物清单***'
   end
 
   def registerItems(items)
-
+    @items = items
   end
+
+  def formatPrice(price)
+    sprintf('%.2f', price)
+  end
+
 end
--- spec/e2e/register_spec.rb
+++ spec/e2e/register_spec.rb
@@ -1,10 +1,11 @@
 
 require 'cash/register'
 require 'cash/product'
+require 'json'
 
 describe Register do
 
-  context 'print normal items' do
+  context 'items with no discount' do
 
     it 'single item' do
 
@@ -20,6 +21,24 @@ describe Register do
       expect(results[1]).to eq '名称:可口可乐,数量:1瓶,单价:3.00(元),小计:3.00(元)'
       expect(results[2]).to eq '------------------'
       expect(results[3]).to eq '总计:3.00(元)'
+      expect(results[4]).to eq '******************'
+
+    end
+
+    it 'item with count' do
+
+      product = Product.new
+      product.id = 'ITEM000001'
+      product.name = '可口可乐'
+      product.unit_price = 3.00
+
+      subject.registerItems([product])
+
+      results = subject.generateReceipts("['ITEM000001-2']")
+      expect(results[0]).to eq '***<没赚钱商店>购物清单***'
+      expect(results[1]).to eq '名称:可口可乐,数量:2瓶,单价:3.00(元),小计:6.00(元)'
+      expect(results[2]).to eq '------------------'
+      expect(results[3]).to eq '总计:6.00(元)'
       expect(results[4]).to eq '******************'
 
     end
